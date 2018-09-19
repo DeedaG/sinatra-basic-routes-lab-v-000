eeec8f123368b4b99ca7b24b6b0db3705c3b27a9
Christia T <christiatidwell@bellsouth.net>
2018-09-19 01:51:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  
+  get '/name' do
+    "My name is Christia."
+  end
+
+  GET '/hometown' do
+    "My hometown is Birmingham, AL."
+  end
+
+  GET '/favorite-song' do
+    "My favorite song is Blue Ain't Your Color."
+  end
 end
